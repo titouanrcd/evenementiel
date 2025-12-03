f44cb8136dd546eb304a43216607c3cb8942c558
MilouPouet <leportabledemarilou@icloud.com>
2025-12-03 13:06:20 +0100

    test footer

--- views/index.php
+++ views/index.php
@@ -206,62 +206,73 @@ $user_role = $_SESSION['user_role'] ?? 'user';
                 </div>
             </div>
         </section>
-        
-        <footer>
-            <div class="footer-content">
-                <div class="footer-col footer-col-main">
-                    <div class="logo footer-logo">NOVA<span>.</span></div>
-                    <p class="footer-description">
-                        Agence événementielle nouvelle génération. Nous créons l'inattendu, nous gérons l'impossible, nous illuminons vos instants.
-                    </p>
-                </div>
 
-                        <div class="social-links">
-        <a href="https://www.instagram.com/" class="social-link instagram" aria-label="Instagram">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
-            <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.5 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-            </svg>
-        </a>
+    <footer>
+        <div class="footer-content">
+            <div class="footer-col footer-col-main">
+                <div class="logo footer-logo">NOVA<span>.</span></div>
+                <p class="footer-description">
+                    Agence événementielle nouvelle génération. Nous créons l'inattendu, nous gérons l'impossible, nous illuminons vos instants.
+                </p>
+            </div>
+            
+            <div class="footer-col">
+                <h5>AGENCE</h5>
+                <ul>
+                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="#">L'Équipe</a></li>
+                    <li><a href="#">Carrières</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
 
-        <a href="https://www.linkedin.com/feed/" class="social-link linkedin" aria-label="LinkedIn">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
-                <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
-            </svg>
-        </a>
+            <div class="footer-col">
+                <h5>EXPERTISE</h5>
+                <ul>
+                    <li><a href="#">Corporate</a></li>
+                    <li><a href="#">Festivals</a></li>
+                    <li><a href="#">Mariages</a></li>
+                    <li><a href="#">Scénographie</a></li>
+                </ul>
+            </div>
 
-        <a href="https://www.tiktok.com/" class="social-link tiktok" aria-label="TikTok">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
-            <path fill="#FFFFFF" d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/>
-            </svg>
-        </a>
-    </div>
-                
-                <div class="footer-col">
-                    <h5>AGENCE</h5>
-                    <ul>
-                        <li><a href="#accueil">Accueil</a></li>
-                        <li><a href="#">L'Équipe</a></li>
-                        <li><a href="#">Carrières</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
+             <div class="social-links">
+    <a href="https://www.instagram.com/" class="social-link instagram" aria-label="Instagram">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+        <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.5 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+        </svg>
+    </a>
 
-                <div class="footer-col">
-                    <h5>EXPERTISE</h5>
-                    <ul>
-                        <li><a href="#">Corporate</a></li>
-                        <li><a href="#">Festivals</a></li>
-                        <li><a href="#">Mariages</a></li>
-                        <li><a href="#">Scénographie</a></li>
-                    </ul>
-                </div>
+    <a href="https://www.linkedin.com/feed/" class="social-link linkedin" aria-label="LinkedIn">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+            <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+        </svg>
+    </a>
+
+    <a href="https://www.tiktok.com/" class="social-link tiktok" aria-label="TikTok">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24">
+        <path fill="#FFFFFF" d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/>
+        </svg>
+    </a>
+</div>
         </div>
         <div class="footer-bottom">
             © 2025 NOVA ÉVÉNEMENTS. Tous droits réservés.
         </div>
     </footer>
-    </main>
 
+    <script>
+        // Mise à jour du prix en temps réel
+        const priceSlider = document.getElementById('priceRange');
+        const priceDisplay = document.getElementById('priceDisplay');
+        
+        if (priceSlider && priceDisplay) {
+            priceSlider.addEventListener('input', function() {
+                const value = parseInt(this.value);
+                priceDisplay.textContent = value >= 200 ? '200€+' : value + '€';
+            });
+        }
+    </script>
     <script src="../js/navbar.js"></script>
-    </body>
+</body>
 </html>
